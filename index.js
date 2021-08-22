ad2e1ff54316abd6d4aaecf9ede58970de99d98c
Pratik Bhadane <pratikbhadane24@gmail.com>
2021-08-22 23:27:30 +0530

    st

--- index.js
+++ index.js
@@ -4,44 +4,37 @@ function makeSounds(val) {
             var crash = new Audio('sounds/crash.mp3');
             crash.play();
             break;
-
         case "a":
             var tom = new Audio('sounds/tom-1.mp3');
             tom.play();
             break;
-
         case 's':
             var snare = new Audio('sounds/snare.mp3');
             snare.play();
             break;
-
         case 'd':
             var tom1 = new Audio('sounds/tom-1.mp3');
             tom1.play();
             break;
-
         case 'j':
             var tom2 = new Audio('sounds/tom-2.mp3');
             tom2.play();
             break;
-
         case 'k':
             var tom3 = new Audio('sounds/tom-3.mp3');
             tom3.play();
             break;
-
         case 'l':
             var tom4 = new Audio('sounds/tom-4.mp3');
             tom4.play();
             break;
-
         default: console.log(val)
     }
 }
 numberofDrumBtns = document.querySelectorAll("button").length
 
 for (var i = 0; i < numberofDrumBtns; i++) {
-    document.querySelectorAll("button")[i].addEventListener("click", function () {
+    document.querySelectorAll("button")[ i ].addEventListener("click", function () {
         var buttonInnerhtml = this.innerHTML;
         makeSounds(buttonInnerhtml);
     });
@@ -53,11 +46,11 @@ document.addEventListener("keypress", function (event) {
 });
 
 function buttonAnimation(currentKey) {
-    var activeButton = document.querySelector("."+currentKey);
+    var activeButton = document.querySelector("." + currentKey);
     activeButton.classList.add("pressed");
 
-    setTimeout(function() {
+    setTimeout(function () {
         activeButton.classList.remove("pressed");
-    },100);
-    
+    }, 100);
+
 }
